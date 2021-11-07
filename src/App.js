0e5c46006f224fb829405c9675e41603a70818da
roy liu <roy.liu@poyue.com.tw>
2021-11-07 23:47:41 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const endpoint =
+    "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+  const initData = async () => {
+    const success = await axios(endpoint);
+    if (success) {
+      console.log(success);
+    }
+  };
+  useEffect(() => {
+    initData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form class="search-form">
+        <input type="text" class="search" placeholder="City or State"></input>
+        <ul class="suggestions">
+          <li>Filter for a city</li>
+          <li>or a state</li>
+        </ul>
+      </form>
     </div>
   );
 }
